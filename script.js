a6eda92bdf1e7c35353954f6cd7f212e210a5326
Fayaz Laghari <lagharifayazali3@gmail.com>
2024-12-12 22:28:23 +0500

    Create a webpage with a 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,15 @@
+const container = document.querySelector(".container")
+const table = document.createElement("table")
+let gridCount = 16
+for(let i= 0 ; i < gridCount ; i++){
+    let tr = document.createElement("tr")
+
+    for (let j = 0; j< gridCount ; j++){
+    let td = document.createElement("td")
+   tr.appendChild(td)
+}
+    table.appendChild(tr)
+}
+container.appendChild(table)
+
+
